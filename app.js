ef1beb4b95252b896947d1413233bd7db48f3478
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-03-18 00:47:12 -0300

    Configuração simples do nome

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto')
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto')
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
 }
 
